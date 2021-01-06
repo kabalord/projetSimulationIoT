31101a802092ef948f489c2efbfe67a8b6036435
kabalord <wroaserrano@gmail.com>
2021-01-06 13:33:55 +0100

    ajoute ancient fichiers

---
+++ portable.py
@@ -0,0 +1,21 @@
+import socket 
+
+socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+host, port = ('', 5566)
+
+try:
+    socket.connect((host, port))
+    print("Client connecté !")
+    
+    data = "Bonjour à toi, je suis le portable ! :)"
+    data = data.encode("utf8")
+    socket.sendall(data)
+        
+    reponse = socket.recv(1024)
+    print(reponse)
+    
+except ConnectionRefusedError:
+    print("Connexion au serveur échouée")
+
+finally:
+    socket.close()
